1b8bf58385dbda92819ca03b6c87db2f58766213
Michał Izworski <m.izworski7@gmail.com>
2017-01-21 00:48:59 +0100

    Parsing expression with lhs, rhs and variables dependent done.

--- circuit.c
+++ circuit.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
 
 #include "circuit.h"
-#include "err.h"
 
 int variables_in[2137];
 int variables_out[2137];
 
-circuit_node *parse_expression(char *expression, size_t expression_length) {
-    if (expression == NULL) {
-        return NULL;
-    }
+void set_variables(unsigned int variables_count, dnode *variables);
 
-    if (expression[0] == '(') {
-        if (expression[1] == '-') {
-            return parse_unary_operation(expression, expression_length);
-        } else {
-            return prase_binary_operation(expression, expression_length);
-        }
-    } else if (*expression == 'x') {
-        return prase_variable(expression);
-    } else {
-        return parse_value(expression);
-    }
-}
+ddag *initialize_dependency_graph(unsigned int count);
 
-circuit_node *prase_binary_operation(char *expression, size_t expression_length) {
-    circuit_node *node = malloc(sizeof(circuit_node));
+void read_parse_equation(unsigned int variables_count, ddag *dependency_graph);
+
+int is_cycled(ddag *dependency_graph);
+
+int dfs_cycle_search(int v, dnode *vertices, unsigned int vertices_visited[]);
+
+enode *
+parse_binary_operation(char *expression,
+                       size_t expression_length,
+                       unsigned int dependent_variables[],
+                       unsigned int variables_count) {
+    enode *node = malloc(sizeof(enode));
 
     char *current_char = expression;
     char *left_expression_begin = expression + 1;
@@ -54,7 +48,7 @@ circuit_node *prase_binary_operation(char *expression, size_t expression_length)
     } while (opened_parentheses != 0 || operator_found == 0);
 
     if (*current_char == '+' || *current_char == '*') {
-        --left_expression_length; /// Counted characters inclucing space and operator
+        --left_expression_length; /// Counted characters includes space and operator
     } else {
         ++left_expression_length; /// Need to add null at the end of the string.
         current_char += 2;
@@ -73,25 +67,28 @@ circuit_node *prase_binary_operation(char *expression, size_t expression_length)
     left_expression[left_expression_length - 1] = 0;
     right_expression[right_expression_length - 1] = 0;
 
-    node->left_son = parse_expression(left_expression, left_expression_length);
-    node->right_son = parse_expression(right_expression, right_expression_length);
+    node->left_son = parse_expression(left_expression, left_expression_length, dependent_variables, variables_count);
+    node->right_son = parse_expression(right_expression, right_expression_length, dependent_variables, variables_count);
 
     return node;
 }
 
-circuit_node *prase_variable(const char *expression) {
-    circuit_node *node = malloc(sizeof(circuit_node));
+enode *parse_variable(const char *expression,
+                      unsigned int dependent_variables[]) {
+    enode *node = malloc(sizeof(enode));
 
     node->operation_code = VARIABLE_CODE;
     node->right_son = NULL;
     node->left_son = NULL;
     sscanf(expression, "x[%d]", &node->value);
 
+    dependent_variables[node->value] = 1;
+
     return node;
 }
 
-circuit_node *parse_value(const char *expression) {
-    circuit_node *node = malloc(sizeof(circuit_node));
+enode *parse_value(const char *expression) {
+    enode *node = malloc(sizeof(enode));
 
     node->operation_code = VALUE_CODE;
     node->right_son = NULL;
@@ -101,32 +98,88 @@ circuit_node *parse_value(const char *expression) {
     return node;
 }
 
-circuit_node *parse_unary_operation(const char *expression, size_t expression_length) {
-    circuit_node *node = malloc(sizeof(circuit_node));
+enode *parse_unary_operation(char *expression,
+                             size_t expression_length,
+                             unsigned int dependent_variables[],
+                             unsigned int variables_count) {
+    enode *node = malloc(sizeof(enode));
 
     char *new_expression = NULL;
     node->operation_code = '-';
     size_t new_expression_length = expression_length - 3;
-    new_expression = malloc(new_expression_length * sizeof(char)); // todo sizeof(char) == 1?
+    new_expression = malloc(new_expression_length * sizeof(char));
     strncpy(new_expression, expression + 2, new_expression_length);
-    node->left_son = parse_expression(new_expression, new_expression_length);
+    node->left_son = parse_expression(new_expression, new_expression_length, dependent_variables, variables_count);
     return node;
+}
+
+enode *parse_expression(char *expression,
+                        size_t expression_length,
+                        unsigned int dependent_variables[],
+                        unsigned int variables_count) {
+    if (expression == NULL) {
+        return NULL;
+    }
+
+    if (expression[0] == '(') {
+        if (expression[1] == '-') {
+            return parse_unary_operation(expression, expression_length, dependent_variables, variables_count);
+        } else {
+            return parse_binary_operation(expression, expression_length, dependent_variables, variables_count);
+        }
+    } else if (*expression == 'x') {
+        return parse_variable(expression, dependent_variables);
+    } else {
+        return parse_value(expression);
+    }
+}
+
+void parse_single_equation(char *expression, size_t expression_length, ddag *dgraph, unsigned int variables_count) {
+    int variable_index;
+    sscanf(expression, "x[%d]", &variable_index);
+
+    unsigned int dependent_variables[variables_count];
+    memset(dependent_variables, 0, sizeof dependent_variables);
+
+    dnode *node = &dgraph->variables[variable_index];
+
+    expression += 7;
+    expression_length -= 7;
+
+    node->expression = parse_expression(expression,
+                                        expression_length,
+                                        dependent_variables,
+                                        variables_count);
+
+    dlist **tail = &node->dependent_variables;
+
+    for (int i = 0; i < variables_count; ++i) {
+        if (dependent_variables[i] == 1) {
+            dlist *new_list = malloc(sizeof(dlist));
+            new_list->variable = &dgraph->variables[i];
+
+            *tail = new_list;
+            tail = &new_list->next;
+        }
+    }
+
+    *tail = NULL;
 }
 
 int main() {
     // todo read input
-    int input_length = 10;
-    int circuit_equations_number = 10;
-    int variables_number = 10;
+    unsigned int input_length = 10;
+    unsigned int circuit_equations_number = 2;
+    unsigned int variables_count = 10;
 
     // PRE-PROCESSING
 
     // todo initialize pipes for variables
     // todo for now all variables pipes are visible to others, can be reduced to initialize them traversing dependency tree
-    int vars_in[variables_number][2];
-    int vars_out[variables_number][2];
+    int vars_in[variables_count][2];
+    int vars_out[variables_count][2];
 
-    for (int i = 0; i < variables_number; ++i) {
+    for (int i = 0; i < variables_count; ++i) {
         if (pipe(vars_in[i]) == -1) {
             syserr("Error in pipe\n");
         }
@@ -136,25 +189,17 @@ int main() {
         }
     }
 
-    // todo parse input to structure
+    ddag *dependency_graph = initialize_dependency_graph(variables_count);
 
-    int which_variable = 1;
+    for (int i = 0; i < circuit_equations_number; ++i) {
+        read_parse_equation(variables_count, dependency_graph);
 
-    char *expression = NULL;
-    size_t len = 0;
-    size_t expression_length;
-    ssize_t read;
+        if (is_cycled(dependency_graph) == 1) {
+            return 42;
+        }
+    }
 
-    read = getline(&expression, &len, stdin);
-
-    // todo remove delimiter from ending, decrease input length
-
-    expression_length = (size_t) read;
-    expression[expression_length - 1] = 0;
-
-    circuit_node *circuit = parse_expression(expression, expression_length);
-
-    // todo create dependency tree
+    printf("Czy zacyklony %d\n", is_cycled(dependency_graph));
 
     // todo initialize processes from structure (create net)
 
@@ -175,4 +220,67 @@ int main() {
     // todo free allocated memory
 
     return 0;
+}
+
+int is_cycled(ddag *dependency_graph) {
+    unsigned int vertices_number = dependency_graph->variables_count;
+    unsigned int vertex_visited[vertices_number];
+
+    memset(vertex_visited, 0, sizeof(vertex_visited));
+
+    for (int v = 0; v < vertices_number; ++v) {
+        if (vertex_visited[v] == 0) {
+            if (dfs_cycle_search(v, dependency_graph->variables, vertex_visited) == 1) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int dfs_cycle_search(int v, dnode *vertices, unsigned int vertices_visited[]) {
+    vertices_visited[v] = 1;
+
+    for (dlist *var = vertices[v].dependent_variables; var != NULL; var = var->next) {
+        int var_index = var->variable->variable_index;
+        if (vertices_visited[var_index] == 1) {
+            return 1;
+        } else if (dfs_cycle_search(var_index, vertices, vertices_visited) == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void read_parse_equation(unsigned int variables_count, ddag *dependency_graph) {
+    char *expression = NULL;
+    size_t len = 0;
+    size_t expression_length;
+    ssize_t read;
+
+    read = getline(&expression, &len, stdin);
+
+    expression_length = (size_t) read;
+    expression[expression_length - 1] = 0;
+
+    parse_single_equation(expression, expression_length, dependency_graph, variables_count);
+}
+
+ddag *initialize_dependency_graph(unsigned int variables_count) {
+    ddag *dag = malloc(sizeof(ddag));
+    dag->variables = malloc(variables_count * sizeof(dnode));
+    dag->variables_count = variables_count;
+    set_variables(variables_count, dag->variables);
+
+    return dag;
+}
+
+void set_variables(unsigned int variables_count, dnode *variables) {
+    for (int i = 0; i < variables_count; ++i) {
+        variables[i].variable_index = i;
+        variables[i].dependent_variables = NULL;
+        variables[i].expression = NULL;
+    }
 }
--- circuit.h
+++ circuit.h
@@ -13,42 +13,67 @@
 #define VARIABLE_CODE 2137
 #define VALUE_CODE 1488
 
-typedef struct circuit_node circuit_node;
+typedef struct enode enode;
+typedef struct cnode cnode;
+typedef struct dependency_dag_node dnode;
+typedef struct dependency_list dlist;
+typedef struct dependency_dag ddag;
 
-struct circuit_node {
+struct enode {
     // todo pipe-in
     // todo pipe-out
 
     int operation_code;
     int value;
-    circuit_node *left_son; /// nodes that you are waiting for
-    circuit_node *right_son;
-    circuit_node *parent; /// nodes to receive your value
+    enode *left_son; /// nodes that you are waiting for
+    enode *right_son;
+    enode *parent; /// nodes to receive your value
 };
 
-typedef struct circuit_variable_node cv_node;
+struct cnode {
+    // todo pipe-in to receive signals
+    // todo pipe-out to output value
 
-struct circuit_variable_node {
-    // todo
-//    int parents_ids[];
+    int variable_index;
+    unsigned int *dependent_variables;
 };
 
-typedef struct dependency_tree_node dt_node;
-
-struct dependency_tree_node {
-    int variable_name;
-    dt_node *left_son;
-    dt_node *right_son;
+struct dependency_dag_node {
+    int variable_index;
+    dlist *dependent_variables;
+    enode *expression;
 };
 
-circuit_node *parse_unary_operation(const char *expression, size_t expression_length);
+struct dependency_list {
+    dnode *variable;
+    dlist *next;
+};
 
-circuit_node *parse_value(const char *expression);
+struct dependency_dag {
+    dnode *variables;
+    unsigned int variables_count;
+};
 
-circuit_node *prase_variable(const char *expression);
+enode *parse_unary_operation(char *expression,
+                             size_t expression_length,
+                             unsigned int dependent_variables[],
+                             unsigned int variables_count);
 
-circuit_node *prase_binary_operation(char *expression, size_t expression_length);
+enode *parse_value(const char *expression);
 
-circuit_node *parse_expression(char *expression, size_t expression_length);
+enode *parse_variable(const char *expression, unsigned int dependent_variables[]);
+
+enode *parse_binary_operation(char *expression,
+                              size_t expression_length,
+                              unsigned int dependent_variables[],
+                              unsigned int variables_count);
+
+enode *parse_expression(char *expression,
+                        size_t expression_length,
+                        unsigned int dependent_variables[],
+                        unsigned int variables_count);
+
+void
+parse_single_equation(char *expression, size_t expression_length, ddag *variables, unsigned int variables_count);
 
 #endif //PWZADANIE3_CIRCUIT_H
